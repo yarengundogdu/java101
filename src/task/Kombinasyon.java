9f94466de62995cb1d402111c68c776131712538
Yaren Gündoğdu <99209245+yarengundogdu@users.noreply.github.com>
2022-08-10 21:36:05 +0300

    Add files via upload

---
+++ src/task/Kombinasyon.java
@@ -0,0 +1,39 @@
+package task;
+
+import java.util.Scanner;
+
+public class Kombinasyon {
+
+    static int fact (int n) {
+        int i, sonuc = 1;
+
+        for (i = 1; i <= n; i++)
+            sonuc *= i;
+
+        return sonuc;
+    }
+
+    public static void main(String[] args) {
+        int c, n, r;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("N'in r'li kombinasyonu için,\nN değeri: ");
+        n = input.nextInt();
+
+        System.out.print("r değeri: ");
+        r = input.nextInt();
+
+        while (n < r) {
+            System.out.println("N değeri r değerinden küçük olamaz. Tekrar deneyiniz.");
+            System.out.print("N değeri: ");
+            n = input.nextInt();
+
+            System.out.print("r değeri: ");
+            r = input.nextInt();
+        }
+
+        c = fact(n) / (fact(r) * fact(n - r));
+
+        System.out.println("C(" + n + ", " + r + ") = " + c);
+    }
+}
